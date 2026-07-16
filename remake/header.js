8af188ff40786dfaf7b55d935d7f2b92c6de88f6
jcolange <jade.colanges@live.fr>
2026-07-16 19:23:59 +0200

    Programmation du header
    - Ajout du sélécteur de langues
    - Préparation du menu principal

---
+++ remake/header.js
@@ -0,0 +1,21 @@
+export const FRENCH_HEADER = {
+  intro: "Développeuse, autrice, traductrice",
+  email: `<i data-lucide="mail"></i> Contactez-moi !`,
+  github: "Voir mes projets sur Github",
+  linkedin: "Voir mon profil LinkedIn",
+  menu_bio: "À propos",
+  menu_skills: "Compétences",
+  menu_projects: "Projets",
+  menu_contact: "Contact",
+};
+
+export const ENGLISH_HEADER = {
+  intro: "Developper, writer, translator",
+  email: `<i data-lucide="mail"></i> Send me an email!`,
+  github: "View my projects on Github",
+  linkedin: "Check my LinkedIn profile",
+  menu_bio: "About me",
+  menu_skills: "Skill",
+  menu_projects: "Projects",
+  menu_contact: "Contact",
+};
